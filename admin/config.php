c159cdb55ed63c88be9e1a0e06825a80f7f0faab
Simon Engelbracht <MetelMC.De@yahoo.com>
2023-12-12 18:11:44 +0100

    Added Config; Added Discord Webhook Support

---
+++ admin/config.php
@@ -0,0 +1,9 @@
+<?php
+    $dsn = 'mysql:dbname=metel_blogging;host=localhost';
+    $username = 'root';
+    $password = '';
+    
+    $con = new PDO($dsn, $username, $password);
+
+    $discord_webhook = "https://discord.com/api/webhooks/1184158643856355458/42F7ffvyCWYJIyz03PKJTxw_l6sEfKTAXyCHZ5jDv-mtzyDIBgdrgrHR26dW24ZqqQKJ";
+?>
